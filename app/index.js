da3e50bcf9f778432df32d47c9ef70d4fce96d77
Andydevs <akanshul97@gmail.com>
2021-04-28 21:36:52 -0400

    Multicolor

--- app/index.js
+++ app/index.js
@@ -8,25 +8,37 @@ import './style/main.scss'
 
 const vertexShaderCode = `
     attribute vec4 aVertexPosition;
+    attribute vec4 aVertexColor;
+
+    varying lowp vec4 vColor;
 
     void main() {
         gl_Position = aVertexPosition;
+        vColor = aVertexColor;
     }
 `
 
 const fragmentShaderCode = `
+    varying lowp vec4 vColor;
+
     void main() {
-        gl_FragColor = vec4(0.0, 0.67, 1.0, 1.0);
+        gl_FragColor = vColor;
     }
 `
 
 // Model vertices!
 const vertices = [
     -0.5, -0.5,
-    0,  0.5,
-    0.5, -0.5
+     0.0,  0.5,
+     0.5, -0.5
 ]
-const numDimensions = 2;
+const numVertexDimensions = 2;
+const colors = [
+    0.0,  0.67, 1.0, 1.0,
+    0.0,  0.0,  0.0, 1.0,
+    0.67, 0.0,  1.0, 1.0
+]
+const numColorDimensions = 4
 
 // Get opengl context
 let canvas = document.getElementById('webgl-canvas')
@@ -64,33 +76,37 @@ if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
 
 // Get shader arguments
 let vertexPosition = gl.getAttribLocation(shaderProgram, 'aVertexPosition')
+let vertexColor = gl.getAttribLocation(shaderProgram, 'aVertexColor')
 
-// Create position buffer
-const positionBuffer = gl.createBuffer();
+// Create buffers
+let positionBuffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-// Add to buffer
-gl.bufferData(gl.ARRAY_BUFFER,
-    new Float32Array(vertices),
-    gl.STATIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+let colorBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW)
 
 // Clear
 function draw() {
-    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    // Opengl clear
+    gl.clearColor(0.0, 0.0, 0.0, 0.0)
     gl.clear(gl.COLOR_BUFFER_BIT)
 
     // Add vertex attribute array
-    let type = gl.FLOAT;
+    let type = gl.FLOAT
     let normalize = false;
     let stride = 0;
     let offset = 0;
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-    gl.vertexAttribPointer(vertexPosition, numDimensions, type, normalize, stride, offset)
+    gl.vertexAttribPointer(vertexPosition, numVertexDimensions, type, normalize, stride, offset)
     gl.enableVertexAttribArray(vertexPosition)
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+    gl.vertexAttribPointer(vertexColor, numColorDimensions, type, normalize, stride, offset)
+    gl.enableVertexAttribArray(vertexColor)
 
     // Fingers crossed this works
     gl.useProgram(shaderProgram)
-    let numPositions = vertices.length / numDimensions
+    let numPositions = vertices.length / numVertexDimensions
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, numPositions)
 }
 draw()
