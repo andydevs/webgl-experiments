58748b6f635516ce84f8cc05d9ced3e9a307dae0
andydevs <akanshul97@gmail.com>
2021-05-13 22:40:22 -0400

    Move shader compilation in separate file

--- app/animation.js
+++ app/animation.js
@@ -5,13 +5,12 @@
  * Created: 04 - 27 - 2021
  */
 import { mat4 } from "gl-matrix"
-import vertexShaderCode from './blue.vs'
-import fragmentShaderCode from './blue.fs'
+import loadShader from './blue.shader'
 import mesh from './mesh.obj'
 
 // Number of components for each
 const VERT_NUM_COMPONENTS = 3
-const NOMR_NUM_COMPONENTS = 3
+const NORM_NUM_COMPONENTS = 3
 
 /**
  * Animation roitine
@@ -21,42 +20,8 @@ const NOMR_NUM_COMPONENTS = 3
 export default function *routine(gl) {
     console.log(mesh)
 
-    // Compile vertex shader
-    let vertexShader = gl.createShader(gl.VERTEX_SHADER)
-    gl.shaderSource(vertexShader, vertexShaderCode)
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.error('An error occurred compiling the vertex shader!')
-        console.error(gl.getShaderInfoLog(vertexShader))
-        gl.deleteShader(vertexShader);
-    }
-
-    // Compile fragment shader
-    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-    gl.shaderSource(fragmentShader, fragmentShaderCode)
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.error('An error occurred compiling the fragment shader!')
-        console.error(gl.getShaderInfoLog(fragmentShader))
-        gl.deleteShader(fragmentShader);
-    }
-
-    // Create shader program
-    let shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram)
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        console.error('An error occurred linking the shader program!')
-        console.error(gl.getProgramInfoLog(shaderProgram))
-    }
-
-    // Get shader parameters
-    let vertexPosition_ = gl.getAttribLocation(shaderProgram, 'aVertexPosition')
-    let vertexNormal_ = gl.getAttribLocation(shaderProgram, 'aVertexNormal')
-    let projectionMatrix_ = gl.getUniformLocation(shaderProgram, 'uProjectionMatrix')
-    let transformMatrix_ = gl.getUniformLocation(shaderProgram, 'uTransformMatrix')
-    let normalMatrix_ = gl.getUniformLocation(shaderProgram, 'uNormalMatrix')
+    // Get shader
+    let shader = loadShader(gl)
 
     // Create buffers
     let positionBuffer = gl.createBuffer()
@@ -82,9 +47,6 @@ export default function *routine(gl) {
         gl.depthFunc(gl.LEQUAL)
         gl.clear(gl.COLOR_BUFFER_BIT)
 
-        // Set program
-        gl.useProgram(shaderProgram)
-
         // Transform matrix
         const transformMatrix = mat4.create()
         mat4.translate(transformMatrix, transformMatrix, [0, 0, -6])
@@ -94,29 +56,35 @@ export default function *routine(gl) {
 
         // Projection matrix
         const projectionMatrix = mat4.create()
-        mat4.perspective(projectionMatrix, Math.PI / 4, gl.canvas.clientWidth / gl.canvas.clientHeight, 1, 100)
+        mat4.perspective(projectionMatrix, 
+            Math.PI / 4, 
+            gl.canvas.clientWidth / gl.canvas.clientHeight, 
+            1, 100)
 
         // Normal matrix
         const normalMatrix = mat4.create()
         mat4.invert(normalMatrix, transformMatrix)
         mat4.transpose(normalMatrix, normalMatrix)
 
+        // Set program
+        gl.useProgram(shader.program)
+
         // Set position attribute
         // type: FLOAT, normalize: false, stride: 0, offset: 0
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-        gl.vertexAttribPointer(vertexPosition_, VERT_NUM_COMPONENTS, gl.FLOAT, false, 0, 0)
-        gl.enableVertexAttribArray(vertexPosition_)
+        gl.vertexAttribPointer(shader.aVertexPosition, VERT_NUM_COMPONENTS, gl.FLOAT, false, 0, 0)
+        gl.enableVertexAttribArray(shader.aVertexPosition)
 
         // Set normal attribute
         // type: FLOAT, normalize: false, stride: 0, offset: 0
         gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer)
-        gl.vertexAttribPointer(vertexNormal_, NOMR_NUM_COMPONENTS, gl.FLOAT, false, 0, 0)
-        gl.enableVertexAttribArray(vertexNormal_)
+        gl.vertexAttribPointer(shader.aVertexNormal, NORM_NUM_COMPONENTS, gl.FLOAT, false, 0, 0)
+        gl.enableVertexAttribArray(shader.aVertexNormal)
 
         // Set uniforms
-        gl.uniformMatrix4fv(projectionMatrix_, false, projectionMatrix)
-        gl.uniformMatrix4fv(transformMatrix_, false, transformMatrix)
-        gl.uniformMatrix4fv(normalMatrix_, false, normalMatrix)
+        gl.uniformMatrix4fv(shader.uProjectionMatrix, false, projectionMatrix)
+        gl.uniformMatrix4fv(shader.uTransformMatrix, false, transformMatrix)
+        gl.uniformMatrix4fv(shader.uNormalMatrix, false, normalMatrix)
         
         // Fingers crossed this works
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
